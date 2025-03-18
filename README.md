f99a65b0464a8f81a77fd4b4d1ff6cba6508469c
Scarlet Rogue <mendozadorantesfernanda6@gmail.com>
2025-03-17 21:59:40 -0600

    Version 1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 <h1>Challenge Amigo Secreto</h1>
+
+<h2>Instrucciones para jugar</h2>
+    /"Agrega un amiguito"/
--- app.js
+++ app.js
@@ -1,1 +1,86 @@
+let amigos = []; 
 
+function agregarAmigo() { 
+    const inputAmigo = document.getElementById("amigo");
+    const nombreAmigo = inputAmigo.value.trim(); 
+
+    if (nombreAmigo === "") { 
+        alert("Por favor ingrese un nombre válido");
+        return;  
+
+    }
+
+    if (amigos.includes (nombreAmigo)) {
+        alert ("Este nombre ya fue agregado");
+        return
+    }
+
+    amigos.push(nombreAmigo);  
+    console.log("Lista actualizada de amigos:", amigos); 
+
+    mostrarAmigos();  
+    inputAmigo.value = "";  
+}
+
+
+function verificarEnter(event) {
+
+    if (event.key === 'Enter') {
+
+        event.preventDefault();
+        
+        agregarAmigo();
+    }
+}
+
+
+function mostrarAmigos() { 
+    const lista = document.getElementById("listaAmigos"); 
+    lista.innerHTML = "";
+
+    amigos.forEach((amigo, index) => { 
+        const li = document.createElement("li"); 
+        li.textContent = amigo; 
+
+        
+        const botonEliminar = document.createElement("button"); 
+        botonEliminar.textContent = " X "; 
+        botonEliminar.classList.add("button-remove"); 
+        botonEliminar.onclick = () => eliminarAmigo(index); 
+
+        li.appendChild(botonEliminar);
+        lista.appendChild(li);
+    });
+}
+
+// Funcion eliminar amigo
+function eliminarAmigo(index) {
+    amigos.splice(index, 1); 
+    mostrarAmigos(); // 
+}
+
+// Función SOLO UN amigo secreto
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("Debe haber al menos 1 amigo en la lista.");
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length); 
+    let amigoSecreto = amigos[indiceAleatorio]; 
+
+    mostrarResultado(amigoSecreto);
+}
+
+// Mostrar resultado 
+function mostrarResultado(amigoSecreto) {
+    const listaResultado = document.getElementById("resultado");
+    listaResultado.innerHTML = ""; 
+
+    const li = document.createElement("li");
+    li.textContent = `🎁 El amigo secreto es: ${amigoSecreto}! 🎉`;
+    listaResultado.appendChild(li);
+}
+
+
+console.log("Lista inicial de amigos:", amigos);
